078c6f41aef42886b2116e9de4837cd5f4558716
Ayumi <s1290026@sshsv212.u-aizu.ac.jp>
2022-07-13 09:31:54 +0900

    Victory message added

---
+++ t3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class HeadsorTails{
+public static void main(String[] args) {
+    int i=0;
+    int j=0;
+    int cnt = 0;
+    int x=0;
+    int heads = 0;
+    
+    Random rnd = new Random();
+    System.out.println("Tossing a coin...");
+    while(cnt<3){
+        System.out.print("Round "+(cnt+1)+":");
+           x =  rnd.nextInt(5)%2;
+        if(x==0) {
+            System.out.println("Heads");
+            i++;
+        }
+        if(x==1) {
+            System.out.println("Tails");
+            j++;
+        }
+        cnt++;
+
+    }
+    System.out.println("Heads: "+i+",Tails:"+j+"");
+    if(i>j) System.out.println("You won");
+    if(i<j) System.out.println("You lost");
+}
+}
+
